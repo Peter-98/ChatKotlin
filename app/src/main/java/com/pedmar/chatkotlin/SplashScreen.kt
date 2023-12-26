ca31c9a3c3b79e932b7a41eed89f849d6ca3207f
Peter-98 <pma751@inlumine.ual.es>
2023-12-26 12:56:44 +0100

    estilo

--- app/src/main/java/com/pedmar/chatkotlin/SplashScreen.kt
+++ app/src/main/java/com/pedmar/chatkotlin/SplashScreen.kt
@@ -4,9 +4,11 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
+import androidx.appcompat.app.AppCompatDelegate
 
 class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
         MostrarBienvenida()
